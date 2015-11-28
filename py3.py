f6c7a9cba7c59cff13d944fda390e7f22f0c1332
gerasymu4ka <nadsca@gmail.com>
2015-11-28 13:19:45 +0200

    Remove blank lines

--- py3.py
+++ py3.py
@@ -1,41 +1,22 @@
-
-
 #Caesar's cypher
-
 #Introduce ROT-X (de)coder
-
 #>>> encode('Hello python!')
-
 #'Hryyb clguba!'
-
 #>>> decode('Hryyb clguba!')
-
 #'Hello python!'
-
 #Bonus: dynamic rotation
 
 
-
-
-
 def encode(text):      
-
     alphabet = ('abcdefghijklmnopqrstuvwxyz')
-
     key = int(raw_input('Enter rotation key (1 - 26): '))
-
     if key == 0 or key > 26:
-
         print('Invalid key!')
-
         key = int(raw_input('Enter rotation key (1 - 26): '))
 
 
-
     before_key = alphabet[:key]
-
     after_key = alphabet[key:]
-
     shifted_alphabet = after_key + before_key
 
 
